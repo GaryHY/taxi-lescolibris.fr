94c44fb95bba204d8f167a59e9be8c9c33e0cc74
gary <henry.gary@hotmail.com>
2023-09-28 23:18:34 +0200

    Add the logo and the different animation

--- src/scripts/hero.js
+++ src/scripts/hero.js
@@ -1,18 +1,27 @@
-const heroBody = document.querySelector(".hero__body");
+// const heroBody = document.querySelector(".hero__body");
+const title = document.querySelector(".hero__title");
+const cards = document.querySelectorAll(".card__element");
 
 const moveBtn = (e) => {
     const buttonContainer = document.querySelector(".button__container");
     if(!e[0].isIntersecting) {
         buttonContainer.classList.add("floating");
+        cards.forEach(card => {
+            card.classList.add("animateCard");
+        })
     } else if (e[0].isIntersecting) {
         buttonContainer.classList.remove("floating");
+        cards.forEach(card => {
+            card.classList.remove("animateCard");
+        })
     }
 }
 
 const moveBtnIntersectObs = new IntersectionObserver(moveBtn);
 
 
-moveBtnIntersectObs.observe(heroBody);
+// moveBtnIntersectObs.observe(heroBody);
+moveBtnIntersectObs.observe(title);
 
 
 
