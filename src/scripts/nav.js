92856da05767a8a7cf6fcf9aac3e5cdb5fe578aa
gary <henry.gary@hotmail.com>
2023-10-02 09:51:59 +0200

    Fix last commit

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -1,12 +1,15 @@
 const primaryNav = document.querySelector("#primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const nav = document.querySelector(".nav");
 
 navToggle.addEventListener("click", ()=>{
     const visible = primaryNav.getAttribute("data-visible");
     if(visible === "true"){
         navToggle.setAttribute("aria-expanded", "false");
         primaryNav.setAttribute("data-visible", "false");
+        nav.style.backgroundColor = "white";
     } else {
+        nav.style.backgroundColor = "red";
         navToggle.setAttribute("aria-expanded", "true");
         primaryNav.setAttribute("data-visible", "true");
     }
