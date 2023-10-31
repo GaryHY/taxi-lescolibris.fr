a8bfa31bc192fbe71b967bbf9f04de12c446429d
gary <henry.gary@hotmail.com>
2023-10-31 09:12:53 +0100

    test de rajouter un console.log

--- functions/form-submitted.js
+++ functions/form-submitted.js
@@ -1,4 +1,5 @@
 exports.handler = async function(event, context){
+    console.log("lancement de la serverless function")
     return {
         statusCode: 200,
         body: JSON.stringify({message: "Je viens en paix"})
