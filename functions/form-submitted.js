92575ffd74d5cba723158d39a64f1485d17022c6
gary <henry.gary@hotmail.com>
2023-10-31 09:23:48 +0100

    Add few changes to add a basic template for netlify serverles form submission

--- functions/form-submitted.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async function(event, context){
-    console.log("lancement de la serverless function")
-    return {
-        statusCode: 200,
-        body: JSON.stringify({message: "Je viens en paix"})
-    } 
-}
---
+++ functions/handleForm/handleForm.js
@@ -0,0 +1,38 @@
+// // optionally configure local env vars
+// require('dotenv').config()
+
+// // details in https://css-tricks.com/using-netlify-forms-and-netlify-functions-to-build-an-email-sign-up-widget
+const process = require('process')
+
+const fetch = require('node-fetch')
+
+const { EMAIL_TOKEN } = process.env
+const handler = async (event) => {
+  const { email } = JSON.parse(event.body).payload
+  console.log(`Received a submission: ${email}`)
+  try {
+    const response = await fetch('https://api.buttondown.email/v1/subscribers', {
+      method: 'POST',
+      headers: {
+        Authorization: `Token ${EMAIL_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email }),
+    })
+    const data = await response.json()
+    console.log(`Submitted to Buttondown:\n ${data}`)
+  } catch (error) {
+    return { statusCode: 422, body: String(error) }
+  }
+}
+
+module.exports = { handler }
+
+
+// exports.handler = async function(event, context){
+//     console.log("lancement de la serverless function")
+//     return {
+//         statusCode: 200,
+//         body: JSON.stringify({message: "Je viens en paix"})
+//     } 
+// }
