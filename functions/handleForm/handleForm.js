e7d75ba6ad96291dbe1e2a1bd853f0141a199aac
gary <henry.gary@hotmail.com>
2023-10-31 20:53:28 +0100

    few changes

--- functions/handleForm/handleForm.js
+++ functions/handleForm/handleForm.js
@@ -12,7 +12,7 @@ const handler = async (event) => {
   // const { formData } = JSON.parse(event.body).payload
   // console.log(`Voici les donnees: ${formData}`)
     console.log(event.body);
-    const formData = JSON.parse(event.body);
+    const formData = JSON.parse(event.body).payload;
     console.log("Les donnees du formulaire de data sont : ", formData);
     const nom = event.body.nom;
     const prenom = event.body.prenom;
