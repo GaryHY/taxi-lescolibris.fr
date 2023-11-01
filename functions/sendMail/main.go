15a22c4b9f43fb0cf2a5bc611e5ec1dbebca06de
gary <henry.gary@hotmail.com>
2023-11-01 20:59:01 +0100

    add the changes to send html in the mail instead of plain text

--- functions/sendMail/main.go
+++ functions/sendMail/main.go
@@ -20,7 +20,10 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*event
 	fmt.Println("Query string parameters of the request", request.QueryStringParameters)
 	fmt.Println("Contenu du body de la requete :", request.Body)
 
-	msg := "Subject: On teste la fonction d'envoi de mail\nThis is the body of the mail."
+	headers := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";"
+	subject := "On teste la fonction d'envoi de mail\nThis is the body of the mail."
+	body := "<h1>Je veux envoyer un nouveau mail en fait les gars</h1>"
+	msg := "Subject: " + subject + headers + "\n\n" + body
 
 	err := smtp.SendMail(
 		"smtp.gmail.com:587",
