ece8dc4d99f674e34a72ab604882a460d41a148f
gary <henry.gary@hotmail.com>
2023-11-04 22:50:04 +0100

    change the duration of the modal apparition

--- src/scripts/form.js
+++ src/scripts/form.js
@@ -125,6 +125,6 @@ form.addEventListener("submit", async (e) => {
         setTimeout(() => {
             modal.close()
             modal.classList.remove("fade");
-        }, 2500);
+        }, 3500);
     }
 })
